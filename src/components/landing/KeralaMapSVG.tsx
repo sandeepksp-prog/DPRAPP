6fa298f03cb5cd6325548f7792998d5019203784
Aadyastyles <SANDEEP.17NOV02@GMAIL.COM>
2026-02-15 18:54:44 +0530

    feat: Force Replace Kerala Map with User V2 Image

--- src/components/landing/KeralaMapSVG.tsx
+++ src/components/landing/KeralaMapSVG.tsx
@@ -5,10 +5,10 @@ import { motion } from "framer-motion";
 export default function KeralaMapSVG() {
     return (
         <div className="w-full h-full flex items-center justify-center relative group p-0 overflow-hidden rounded-xl bg-white">
-            {/* USER PROVIDED KERALA MAP IMAGE */}
+            {/* USER PROVIDED KERALA MAP IMAGE - VERSION 2 */}
             <div className="relative w-full h-full">
                 <img
-                    src="/assets/kerala_final_map.jpg"
+                    src="/assets/kerala_final_map_v2.jpg"
                     alt="Kerala District Map - Alappuzha"
                     className="w-full h-full object-contain object-center transition-transform duration-700 hover:scale-105"
                 />
